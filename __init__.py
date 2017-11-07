23ec3a7262b406db4ea8336c0622f4047a75b4a9
polyvios <polyvios@ics.forth.gr>
2017-11-07 20:29:11 +0200

    add __init__

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+###########################################
+# (c) 2016-2017 Polyvios Pratikakis
+# polyvios@ics.forth.gr
+###########################################
+
+__all__ = ['greekdict']
